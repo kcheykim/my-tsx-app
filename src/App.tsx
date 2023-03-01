906724c42d0ed682c21526d37a3ea074ca989874
kcheykim <kosalcheykim@gmail.com>
2023-02-28 23:01:14 -0800

    adding object array

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+//useState is use to store data inside a component
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [people, setPeople] = useState([{
+    name: "LeBron James",
+    url: "",
+    age: 36,
+    note: "Guess what? Stop whining."
+    },
+    {
+    name: "Kobe Bryant",
+    url: "",
+    age: 36,
+    }
+  ]);
+
+  people.map(person => {
+    person.age
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited To My Party</h1>
     </div>
   );
 }
